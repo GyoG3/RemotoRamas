b96a57fac6f5269569c44549c76d725e20be0cc9
Gueorqui Orozco <g.orozco3@gmail.com>
2022-05-27 11:46:43 +0200

    Snap 1 master

---
+++ src/principal/Principal.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package principal;
+
+/**
+ *
+ * @author Curso Mañana
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public double sumar (double a, double b){
+    return 1.0;
+    }
+    
+    public double multiplicar (double a, double b){
+    return 1.0;
+    }
+    
+}
